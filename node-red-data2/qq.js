3ad275d742e3e25920a4580b055f1133f5ef45d0
Александр Рудин <ax.rudin@gmail.com>
2022-10-05 19:18:46 +0300

    1

---
+++ node-red-data2/qq.js
@@ -0,0 +1,291 @@
+[{
+    "closedByEmployee": null,
+    "resolutionCode": null,
+    "addArtikle": false,
+    "solvedByTeam": null,
+    "spentTime": null,
+    "jobTitleEmp": "111111111111",
+    "bpmRoute": {
+        "UUID": "bpm$135590104",
+        "title": "Новый сотрудник (export)",
+        "metaClass": "bpm$route"
+    },
+    "agreementUser": null,
+    "Zayavka": [],
+    "timeAllowanceTimer": {
+        "status": "ACTIVE",
+        "deadLineTime": "2022.10.05 13:58:27",
+        "allowance": 2556040,
+        "elapsedFromOverdue": 0
+    },
+    "number": 813091,
+    "closedByTeam": null,
+    "testSpec_ou": null,
+    "state": "registered",
+    "bindedSC2": [],
+    "reportsOff": false,
+    "tasks": [],
+    "reactionTime": {
+        "status": "ACTIVE",
+        "elapsed": 26243952
+    },
+    "agreement": {
+        "UUID": "agreement$605301",
+        "title": "Департамент ИТ",
+        "metaClass": "agreement$agreement"
+    },
+    "subscribers": [],
+    "bindedToSC": [],
+    "impact": {
+        "UUID": "impact$137002",
+        "title": "3. Низкая",
+        "metaClass": "impact",
+        "code": "I0"
+    },
+    "clientEmployee": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "ClientAnswerWT": {
+        "status": "NOTSTARTED",
+        "elapsed": 0
+    },
+    "timeZone": {
+        "UUID": "timezone$4501",
+        "title": "Москва",
+        "metaClass": "timezone",
+        "code": "Europe/Moscow"
+    },
+    "clientLinkName": "Коновалова Анастасия Александровна / Департамент ИТ / Подключение нового сотрудника",
+    "priority": {
+        "UUID": "priority$5901",
+        "title": "4. Минимальный",
+        "metaClass": "priority",
+        "code": "normal"
+    },
+    "serviceTime": {
+        "UUID": "servicetime$112727801",
+        "title": "8x5",
+        "metaClass": "servicetime",
+        "code": "class_8x5_24.10.2013 15:44:30"
+    },
+    "slaved": false,
+    "BuyWaitTime": {
+        "status": "NOTSTARTED",
+        "elapsed": 0
+    },
+    "createUp": null,
+    "shortDescr": "Подключение нового сотрудника 44444444 3333333333 222222222222",
+    "rfcReason": [],
+    "startDate": null,
+    "subsUnlicensed": [],
+    "clientName": "Коновалова Анастасия Александровна",
+    "coloOverdue": {
+        "UUID": "overDue$1522601",
+        "title": "не просрочен",
+        "metaClass": "overDue"
+    },
+    "icon": null,
+    "dateDecision": null,
+    "authorDouble": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "overdue": {
+        "status": "ACTIVE",
+        "deadLineTime": "2022.10.05 13:58:27",
+        "allowance": 2556053,
+        "elapsedFromOverdue": 0
+    },
+    "postoponeTill": null,
+    "requestDate": "2022.10.04 14:57:52",
+    "KEs": [],
+    "testSpec_em": null,
+    "startTime": "2022.10.04 14:58:27",
+    "solvedByEmployee": null,
+    "categories": [],
+    "resolutionTime": {
+        "length": 8,
+        "interval": "HOUR"
+    },
+    "commentAdded": false,
+    "createBelow": [],
+    "testSpec_te": null,
+    "descriptionInRTF": null,
+    "descriptionRTF": "<div>вауыуауываы</div>",
+    "reaction": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "newEmployee": "44444444 3333333333 222222222222<br />111111111111<br />Группа сопровождения программного обеспечения",
+    "baseCategory": null,
+    "resumptionSum": 0,
+    "removed": false,
+    "emailsFromCC": null,
+    "RespTeam2": {
+        "UUID": "team$102323702",
+        "title": "IT | 1-я линия технической поддержки",
+        "metaClass": "team$team"
+    },
+    "clientTeam": null,
+    "folders": [],
+    "deadLineTime": "2022.10.05 13:58:27",
+    "waitingTime": null,
+    "ActualDate": "2022.10.04 00:00:00",
+    "workRecords": [],
+    "wfProfile": null,
+    "KBtoAddArticle": null,
+    "registrationDate": "2022.10.04 14:58:27",
+    "UUID": "serviceCall$143625028",
+    "resultDescr": null,
+    "bpmTemplate": null,
+    "clientEmail": "a.konovalova@berg.ru",
+    "ClientService": null,
+    "InWorkTime": {
+        "status": "NOTSTARTED",
+        "elapsed": 0
+    },
+    "author": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "reactDeadline": {
+        "status": "EXCEED",
+        "deadLineTime": "2022.10.05 07:58:27",
+        "allowance": 0,
+        "elapsedFromOverdue": 1763962
+    },
+    "surNameEmp": "44444444",
+    "creationDate": "2022.10.04 14:58:27",
+    "wayAddressing": {
+        "UUID": "wayAdressing$647202",
+        "title": "Личный кабинет",
+        "metaClass": "wayAdressing",
+        "code": "SD"
+    },
+    "clientPhone": null,
+    "BeforeWorkStar": {
+        "status": "ACTIVE",
+        "elapsed": 26243995
+    },
+    "responsibleTeam": {
+        "UUID": "team$102323702",
+        "title": "IT | 1-я линия технической поддержки",
+        "metaClass": "team$team"
+    },
+    "RespEmploy2": null,
+    "titleKB": null,
+    "resumed": false,
+    "problems": [],
+    "closedBy": null,
+    "metaClass": "serviceCall$newEmploeeSC",
+    "NameEmp": "3333333333",
+    "codeOfClosing": null,
+    "solvedBy": null,
+    "testSpec": null,
+    "description": "Описание",
+    "FailCheck": false,
+    "title": "813091",
+    "stateStartTime": "2022.10.04 14:58:27",
+    "reactAllow": {
+        "status": "EXCEED",
+        "deadLineTime": "2022.10.05 07:58:27",
+        "allowance": 0,
+        "elapsedFromOverdue": 1763975
+    },
+    "assesmentDate": null,
+    "DelayedFor": {
+        "status": "NOTSTARTED",
+        "elapsed": 0
+    },
+    "clientOU": {
+        "UUID": "ou$2266805",
+        "title": "Группа сопровождения программного обеспечения",
+        "metaClass": "ou$ou"
+    },
+    "massProblem": false,
+    "urgency": {
+        "UUID": "urgency$37503",
+        "title": "3.Низкая",
+        "metaClass": "urgency",
+        "code": "U2"
+    },
+    "middleNameEmp": "222222222222",
+    "responsible": {
+        "UUID": "team$102323702",
+        "title": "IT | 1-я линия технической поддержки",
+        "metaClass": "team$team"
+    },
+    "client": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "IncompCall": false,
+    "reaction_em": {
+        "UUID": "employee$117609720",
+        "title": "Коновалова Анастасия Александровна",
+        "metaClass": "employee$employee"
+    },
+    "department": {
+        "UUID": "ou$2266805",
+        "title": "Группа сопровождения программного обеспечения",
+        "metaClass": "ou$ou"
+    },
+    "bpmBind": null,
+    "timeClosing24": null,
+    "lastModifiedDate": "2022.10.05 12:46:27",
+    "reaction_te": {
+        "UUID": "team$558101",
+        "title": "IT | 2-я линия поддержки ИС",
+        "metaClass": "team$team"
+    },
+    "timeForAccept": null,
+    "totalTimeTimer": {
+        "status": "ACTIVE",
+        "elapsed": 26243974
+    },
+    "queuedTimeAttr": {
+        "status": "NOTSTARTED",
+        "elapsed": 0
+    },
+    "processingTimeTimer": {
+        "status": "ACTIVE",
+        "elapsed": 26243973
+    },
+    "reactTime": {
+        "length": 2,
+        "interval": "HOUR"
+    },
+    "negotiations": [],
+    "system_icon": null,
+    "removalDate": null,
+    "masterMassProblem": null,
+    "service": {
+        "UUID": "slmService$133968701",
+        "title": "Подключение нового сотрудника",
+        "metaClass": "slmService$slmService"
+    },
+    "compliance": {
+        "UUID": "compliance$2950902",
+        "title": "1. Менее 15%",
+        "metaClass": "compliance",
+        "code": "15"
+    },
+    "reactOverdue": {
+        "status": "EXCEED",
+        "deadLineTime": "2022.10.05 07:58:27",
+        "allowance": 0,
+        "elapsedFromOverdue": 1763984
+    },
+    "rfcResults": [],
+    "massProblemSlaves": [],
+    "reverseBPM": [],
+    "responsibleStartTime": "2022.10.04 14:58:27",
+    "mark": null,
+    "responsibleEmployee": null
+}]
